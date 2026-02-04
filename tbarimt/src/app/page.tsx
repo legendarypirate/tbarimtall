b7292053635d89157db3df3113b2525dd08903a1
bayakaa <bayakaa@bayakaas-MacBook-Pro.local>
2026-02-04 15:34:56 +0800

    Stop tracking delivery package files

--- tbarimt/src/app/page.tsx
+++ tbarimt/src/app/page.tsx
@@ -1820,7 +1820,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-8">
               {recentProducts.map((product) => {
                 return (
                   <div
